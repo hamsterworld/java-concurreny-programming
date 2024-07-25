cdda3d987cc1a981dbeac3f35df157a00e1edb2c
isoh <ssoboro1@Gmail.com>
2024-07-26 00:22:17 +0900

    concurrency programming

--- src/main/java/io/concurrency/chapter11/exam05/ThenApplyExample.java
+++ src/main/java/io/concurrency/chapter11/exam05/ThenApplyExample.java
@@ -3,32 +3,34 @@ package io.concurrency.chapter11.exam05;
 import java.util.concurrent.CompletableFuture;
 
 public class ThenApplyExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         MyService myService = new MyService();
         long started = System.currentTimeMillis();
-        CompletableFuture<Integer> asyncFuture = CompletableFuture.supplyAsync(() -> {
-                                                    System.out.println("thread1:" + Thread.currentThread().getName());
-                                                    try {
-                                                        Thread.sleep(500);
-                                                    } catch (InterruptedException e) {
-                                                        throw new RuntimeException(e);
-                                                    }
-                                                    return 40;
-                                                })
-                                                .thenApply(result -> {
-                                                    System.out.println("thread2:" + Thread.currentThread().getName());
-                                                    int r = myService.getData1();
-                                                    return r + result;
-                                                }).thenApplyAsync(result -> {
-                                                    System.out.println("thread3:" + Thread.currentThread().getName());
-                                                    int r = myService.getData2();
-                                                    return r + result;
-                                                });
+        CompletableFuture<Integer> asyncFuture = CompletableFuture
+                .supplyAsync(() -> {
+                    System.out.println("thread1:" + Thread.currentThread().getName());
+//                    try {
+//                        Thread.sleep(500);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+                    return 40;
+                })
+                .thenApply(result -> {
+                    System.out.println("thread2:" + Thread.currentThread().getName());
+                    int r = myService.getData1();
+                    return r + result;
+                }).thenApplyAsync(result -> {
+                    System.out.println("thread3:" + Thread.currentThread().getName());
+                    int r = myService.getData2();
+                    return r + result;
+                });
 
-        int asyncResult = asyncFuture.join();
-        System.out.println("final result: " + asyncResult);
+//        int asyncResult = asyncFuture.join();
+//        System.out.println("final result: " + asyncResult);
         System.out.println("총 소요 시간: " + (System.currentTimeMillis() - started));
+//        Thread.sleep(2000L);
     }
 
     static class MyService {
