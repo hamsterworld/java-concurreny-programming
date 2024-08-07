e8567a5a5c4901d0fa8031624d527867d884ff65
isoh <ssoboro1@Gmail.com>
2024-08-08 00:45:37 +0900

    concurrency programming

--- src/main/java/io/concurrency/chapter04/exam01/ThreadExceptionExample.java
+++ src/main/java/io/concurrency/chapter04/exam01/ThreadExceptionExample.java
@@ -4,16 +4,20 @@ public class ThreadExceptionExample {
 
     public static void main(String[] args) {
 
+        System.err.println("안녕");
+
         try {
             new Thread(() -> {
+                // 이거 err 출력이라 실제로 log 에 안남을걸?
                 throw new RuntimeException("스레드 1 예외!");
             }).start();
-        }catch(Exception e){
+        } catch(Exception e){
             sendNotificationToAdmin(e);
         }
 
     }
 
+    // 여기로 안옴
     private static void sendNotificationToAdmin(Throwable e) {
         System.out.println("관리자에게 알림: " + e.getMessage());
     }
